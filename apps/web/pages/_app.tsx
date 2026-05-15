ac60e49d7b38f9cd9132b99f0643eda35d64dbe1
Albin Norback <albin.nojback@gmail.com>
2026-05-15 22:52:09 +0200

    add speed insights

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,6 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import { ConfigProvider } from "../lib/contexts/ConfigContext";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({
   Component,
@@ -12,6 +13,7 @@ export default function App({
       <ConfigProvider>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </ConfigProvider>
     </SessionProvider>
   );
